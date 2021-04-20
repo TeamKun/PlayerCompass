a77f2a4bef2955846b95ee92fba3ba3ee1f62479
Maru <maru043@docomo.ne.jp>
2021-04-21 06:12:42 +0900

    playerpositionコマンド実装

--- src/main/java/net/kunmc/lab/playercompassplugin/PositionCommand.java
+++ src/main/java/net/kunmc/lab/playercompassplugin/PositionCommand.java
@@ -1,7 +1,12 @@
 package net.kunmc.lab.playercompassplugin;
 
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -23,6 +28,10 @@ public class PositionCommand implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + targetName + "はログインしていません.");
             return true;
         }
+        Location loc = target.getLocation();
+
+        Audience audience = ((Audience) sender);
+        audience.sendActionBar(Component.text(String.format("%sの座標 X:%.0f Y:%.0f Z:%.0f", targetName, loc.getX(), loc.getY(), loc.getZ())));
         return true;
     }
 }
