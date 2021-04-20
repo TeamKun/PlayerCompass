d095dcc60624d21d3afb49b4e168a9f8520a20be
Maru <maru043@docomo.ne.jp>
2021-04-21 04:55:05 +0900

    changeupdateperiodコマンドのバグ修正

--- src/main/java/net/kunmc/lab/playercompassplugin/ChangeUpdatePeriodCommand.java
+++ src/main/java/net/kunmc/lab/playercompassplugin/ChangeUpdatePeriodCommand.java
@@ -18,9 +18,14 @@ public class ChangeUpdatePeriodCommand implements CommandExecutor, TabCompleter 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length < 1) return false;
-
-        long period = Long.getLong(args[0]);
-        if (period < 0) {
+        long period;
+        try {
+            period = Long.parseLong(args[0]);
+            if (period < 0) {
+                sender.sendMessage(ChatColor.RED+"<period>は0以上の数値を指定してください.");
+                return true;
+            }
+        } catch (NumberFormatException e) {
             sender.sendMessage(ChatColor.RED+"<period>は0以上の数値を指定してください.");
             return true;
         }
